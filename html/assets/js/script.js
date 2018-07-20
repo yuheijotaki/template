b3809a582fdc2fc98cc06af05878fc7fce9d1d03
yuheijotaki <yuheijotaki@gmail.com>
2018-07-20 11:07:55 +0900

    [change] HTML template

---
+++ html/assets/js/script.js
@@ -0,0 +1,21 @@
+// hoge
+function hoge() {
+}
+
+
+
+$(function(){
+  hoge();
+}); // ready
+
+$(window).on('load', function(){
+}); // load
+
+$(window).on('resize', function(){
+}); // resize
+
+$(window).on('scroll', function(){
+}); // scroll
+
+$(window).on('load resize', function(){
+}); // load resize
